ce31021c01aa97964f350a65d9e2ad5e50c6a6da
huzaifa2202 <huzaifaqureshi391@gmail.com>
2024-11-17 14:20:36 -0800

    index.html

---
+++ ap.js
@@ -0,0 +1,22 @@
+var input = document.getElementById('inp')
+console.log(input)
+
+function setANumber(num){
+    console.log('number', num)
+    input.value += num
+}
+
+function allClear(){
+    input.value = ""
+}
+
+function calc(){
+    console.log(input.value)
+    var output = eval(input.value)
+    console.log(output)
+    input.value = output
+}
+
+function deleteLast() {
+    input.value = input.value.slice(0, -1); 
+}
